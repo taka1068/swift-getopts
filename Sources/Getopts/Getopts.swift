10041039116a31bcd64f8fecab63928212bad5a8
TakanoriHirobe <london.2xxx@gmail.com>
2020-02-22 23:25:03 +0900

    remove template

--- Sources/Getopts/Getopts.swift
+++ Sources/Getopts/Getopts.swift
@@ -1,3 +1,1 @@
-struct Getopts {
-    var text = "Hello, World!"
-}
+
--- Tests/GetoptsTests/GetoptsTests.swift
+++ Tests/GetoptsTests/GetoptsTests.swift
@@ -6,7 +6,6 @@ final class GetoptsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Getopts().text, "Hello, World!")
     }
 
     static var allTests = [
