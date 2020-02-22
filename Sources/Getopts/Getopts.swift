982335f3840e0572275be41c6b9b110780756129
TakanoriHirobe <london.2xxx@gmail.com>
2020-02-23 00:16:41 +0900

    parsing style

--- Sources/Getopts/Getopts.swift
+++ Sources/Getopts/Getopts.swift
@@ -1,1 +1,9 @@
 
+public enum ParsingStyle {
+    /// Option and non-option arguments can be freely intermixed.
+    /// This allows options to be given in any order.
+    case anyOrder
+    /// As soon as non-option argument is encountered, stop
+    /// considering any remaining arguments as options.
+    case stopAtFirstNonOption
+}
