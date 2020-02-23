5042cc6abef24bf7ae278d9ba5d7af49064fde86
TakanoriHirobe <london.2xxx@gmail.com>
2020-02-24 02:01:55 +0900

    Options

--- Sources/Getopts/Getopts.swift
+++ Sources/Getopts/Getopts.swift
@@ -7,3 +7,34 @@ public enum ParsingStyle {
     /// considering any remaining arguments as options.
     case stopAtFirstNonOption
 }
+
+/// A description of the options that a program can handle.
+public struct Options {
+    let options: [Options]
+    let parsingStyle: ParsingStyle
+    let longOnly: Bool
+}
+
+/// Represents an option.
+public struct Option {
+    /// Short name of the option, e.g. `h` for a `-h` option.
+    let shortName: Character?
+    /// Long name of the option, e.g. `help` for a `--help` option.
+    let longName: String?
+    /// Hint for argument, e.g. `FILE` for a `-o FILE` option.
+    let hint: String?
+    /// Description for usage help text.
+    let description: String?
+    /// Whether option has an argument.
+    let hasArgument: HasArgument
+}
+
+/// Describes whether an option has an argument
+public enum HasArgument {
+    /// The option requires as argument.
+    case yes
+    /// The option takes no argument.
+    case no
+    /// The option argument is optional.
+    case optional
+}
